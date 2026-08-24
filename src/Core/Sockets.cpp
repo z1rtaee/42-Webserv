b05d8ccb4c34dc1cfab0d1aa6367207c0e0715de
jlima-olive <jlima.olive@gmail.com>
2026-08-24 02:23:31 +0100

    Ready to work

--- src/Core/Sockets.cpp
+++ src/Core/Sockets.cpp
@@ -33,7 +33,7 @@ void	Sockets::ClientRequest(int ind)
 
 	bread = read (AllSockets[ind].fd, Rec, BUFFER_SIZE);
 	Rec[bread] = '\0';
-	// Client->request.parseRequest(Rec);	
+	Client->request.parseRequest(Rec);	
 }
 
 const char *STDHTTPResponse()
